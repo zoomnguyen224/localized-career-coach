235cfc8784ce9262980e1dcdeb846d851ddba759
zoomnguyen224 <zoom.nguyen224@gmail.com>
2026-04-23 12:00:54 -0400

    refactor(shell): move routes under (shell), retire (app) layout and sidebar

--- src/app/(app)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { AppLayoutClient } from '@/components/layout/AppLayoutClient'
-
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return <AppLayoutClient>{children}</AppLayoutClient>
-}
